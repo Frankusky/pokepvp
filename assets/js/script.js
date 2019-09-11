b03ec04503df91f6de69687fdaedfc76c0ee0d44
Frank <frank.sam@autoweb.com>
2019-09-11 09:42:36 -0600

    Returning types inside span element

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,35 @@
         }
         return selectedTypes
     }
-    
-    document.querySelector(".randomChooseBtn").addEventListener("click", function(){
-        document.querySelector(".types").innerHTML = randomThreeItemsChooser().join(", ")
+
+    document.querySelector(".randomChooseBtn").addEventListener("click", function () {
+        document.querySelector(".types").innerHTML = "";
+        randomThreeItemsChooser().forEach(function(type,index){
+            document.querySelector(".types").innerHTML += `<span class="${type}">${type}</span>`
+        })
     })
+
+    /*
+        PUTO el que modifique esto ilegalmente >:v
+    */
+//    let endpoint = atob("aHR0cHM6Ly93d3cuanNvbnN0b3JlLmlvLzNhNzE2ZWMyZTI4MDZlOTZlMGIxOWI3MGUzNWYxZGU4NzAyZjFhNDQ0YWIwMTJlNWI2MmQ5MjM4M2JmOTNiYWU=");
+//    fetch(endpoint, {
+//        headers: {
+//            'Content-type': 'application/json',
+//            'Origin' : window.location.href
+//        },
+//        method: 'POST',
+//        body: JSON.stringify({
+//            name: 'jon snow',
+//            age: 31
+//        }),
+//    });
+//
+//    fetch(endpoint, {
+//        method: "GET",
+//        headers: {
+//            'Content-Type': 'application/json',
+//            'Origin' : window.location.href
+//        }
+//    }).then(res => res.json()).then(res => console.log(res))
 })()
